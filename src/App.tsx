35c30a7317cddf1e4ea00480437d2bf74b091b17
luke-davison <luke.warmwater6@gmail.com>
2020-10-13 23:38:30 +1300

    Variety of improvements that I should have split into smaller commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+
 import "./App.css";
+
 import { Form } from "./components/Form";
 
 function App() {
   return (
     <div className="app">
-      <header className="app__header">
-        <Form />
-      </header>
+      <Form />
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,44 +1,45 @@
 import React from "react";
+
 import "./Form.css";
 
-export class Form extends React.Component {
+export interface IFormState {
+  firstName: string
+  lastName: string
+}
+
+export class Form extends React.Component<{}, IFormState> {
   state = {
     firstName: "",
     lastName: ""
   };
 
-  onSubmit() {
+  onSubmit = (evt: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault()
     console.log(this.state);
   }
 
   render() {
-    var title = "New Form";
     return (
-      <div>
-        <div>{title}</div>
-        <form className="form clearfix">
+      <div className="form-container">
+        <h2 className="form-title">
+          New Form
+        </h2>
+        <form className="form" onSubmit={ this.onSubmit }>
           <input
             placeholder="First name"
             name="firstName"
             className="form-control form-input"
-            onChange={evt => {
-              this.setState({ firstName: evt.target.value });
-            }}
+            onChange={ evt => this.setState({ firstName: evt.target.value }) }
           />
           <input
             placeholder="Last name"
             name="lastName"
             className="form-control form-input"
-            onChange={evt => {
-              this.setState({ lastName: evt.target.value });
-            }}
+            onChange={ evt => this.setState({ lastName: evt.target.value }) }
           />
+          <input type="submit" style={{ display: "none" }}/>
         </form>
-        <button
-          type="button"
-          className="btn btn-primary form-btn"
-          onClick={this.onSubmit}
-        >
+        <button className="btn btn-primary form-btn" onClick={ this.onSubmit }>
           Save
         </button>
       </div>
